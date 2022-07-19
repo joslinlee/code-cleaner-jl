f85618e7af3c4c03326e5fa69e5fd3823d6a8d0e
Francisco Reynoso <fjreynoso@pima.edu>
2022-07-18 17:27:51 -0700

    Added rules to remove empty elements and ones containing &nbsp;

--- gulpfile.js
+++ gulpfile.js
@@ -5,35 +5,65 @@ const beautify = require("gulp-jsbeautifier");
 // Say Hi
 gulp.task("default", async () => {
   return console.log(`
-      Hello Human.
+
+      Hello, human.
+      For full list of commands use: npm run info
+  
+    `);
+});
+
+gulp.task("info", async () => {
+  return console.log(`
+
+      Clean code: npm run clean
+      Log error: npm run error
+  
     `);
 });
 
 gulp.task("clean", async () => {
   return gulp.src("_input/**/*.html")
-  .pipe(dom(function() {
-    const discardTextAttributes = (tableElement, ...tableAttributes) => tableAttributes.forEach((tableAttribute) => tableElement.removeAttribute(tableAttribute));
-    return this.querySelectorAll("h1, h2, h3, h4, h5, h6, p, ul, ol, li, dl, dt, dd").forEach((tableElem) => discardTextAttributes(tableElem, "width", "style"));
+  // remove elements with "&nbsp" as inner-text
+  .pipe(dom(function () {
+    const par = this.querySelectorAll("p")
+    return par.forEach((e) => {
+      if(e.textContent == "\xa0") e.remove()
+    })
+  }))
+  // remove empty elements
+  .pipe(dom(function () {
+    const par = this.querySelectorAll("div, span, h1, h2, h3, h4, h5, h6, p, strong")
+    return par.forEach((e) => {
+      if(e.textContent == "") e.remove()
+    })
   }))
   .pipe(dom(function() {
-    const discardElemAttributes = (tableElement, ...tableAttributes) => tableAttributes.forEach((tableAttribute) => tableElement.removeAttribute(tableAttribute));
-    return this.querySelectorAll("div, span").forEach((tableElem) => discardElemAttributes(tableElem, "width", "style"));
+    const discardTextAttributes = (textElement, ...attributes) => attributes.forEach((attribute) => textElement.removeAttribute(attribute));
+    return this.querySelectorAll("h1, h2, h3, h4, h5, h6, p, ul, ol, li, dl, dt, dd").forEach((elem) => discardTextAttributes(elem, "width", "style"));
+  }))
+  .pipe(dom(function() {
+    const discardElemAttributes = (element, ...attributes) => attributes.forEach((attribute) => element.removeAttribute(attribute));
+    return this.querySelectorAll("body, div, span, bold, em").forEach((elem) => discardElemAttributes(elem, "style"));
   }))
   .pipe(dom(function() {
     const discardTableAttributes = (tableElement, ...tableAttributes) => tableAttributes.forEach((tableAttribute) => tableElement.removeAttribute(tableAttribute));
     return this.querySelectorAll("table, thead, tbody, tfoot, tr, th, td").forEach((tableElem) => discardTableAttributes(tableElem, "cellspacing", "cellpadding", "width", "style"));
   }))
   .pipe(dom(function() {
-      const discardTargetSelf = (tableElement, ...tableAttributes) => tableAttributes.forEach((tableAttribute) => tableElement.removeAttribute(tableAttribute));
-      return this.querySelectorAll('[target="_self"]').forEach((tableElem) => discardTargetSelf(tableElem, "target"));
+      const discardTargetSelf = (element, ...attributes) => attributes.forEach((attribute) => element.removeAttribute(attribute));
+      return this.querySelectorAll('[target="_self"], [target="_new"]').forEach((tableElem) => discardTargetSelf(tableElem, "target"));
     }))
     .pipe(dom(function() {
-      const discardRolePres = (tableElement, ...tableAttributes) => tableAttributes.forEach((tableAttribute) => tableElement.removeAttribute(tableAttribute));
+      const discardRolePres = (element, ...attributes) => attributes.forEach((attribute) => element.removeAttribute(attribute));
       return this.querySelectorAll('[role="presentation"]').forEach((tableElem) => discardRolePres(tableElem, "role"));
     }))
     .pipe(beautify({ 
       indent_size: 2, 
-      wrap_attributes: false 
+      wrap_attributes: false,
+      extra_liners: [] 
     }))
+    .pipe(beautify.reporter())
     .pipe(gulp.dest("_output"));
 });
+
+//remove empty elements
