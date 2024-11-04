220695683123ac81960bc18d2d2c2eefe6f6973b
Clint Mossman <cmossman@pima.edu>
2024-11-04 08:34:53 -0700

    Changed if statements for how h1 errors are shown

--- modules/log/checkHeadings.js
+++ modules/log/checkHeadings.js
@@ -2,13 +2,24 @@ export function checkHeadings(document, filePath, errors) {
   const headings = Array.from(document.querySelectorAll("h1, h2, h3, h4, h5, h6"));
   let previousLevel = 0;
 	let h1Count = 0;
+	const h1Check = document.querySelector("h1");
 
 	// Check if <h1> is present as the document title and <h2> follows
-	if (!headings.length || headings[0].tagName !== "H1") {
+	if (headings.length && h1Check && headings[0].tagName !== "H1") {
 		if (!errors[filePath]) {
 			errors[filePath] = [];
 		}
 		errors[filePath].push(`Invalid heading structure (document must start with an <h1> heading).`);
+	} else if (!headings.length) {
+		if (!errors[filePath]) {
+			errors[filePath] = [];
+		}
+		errors[filePath].push(`Invalid heading structure (no headings found).`);
+	} else if (headings.length && !h1Check) {
+		if (!errors[filePath]) {
+			errors[filePath] = [];
+		}
+		errors[filePath].push(`Invalid heading structure (no <h1> heading found).`);
 	}
 
 	// Loop through each heading and check for errors
