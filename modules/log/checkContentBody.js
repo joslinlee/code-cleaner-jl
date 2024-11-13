3177c6512755b6e914655afd8cc63f0695657f31
Joslin Lee <jlee127@pima.com>
2024-11-12 17:21:13 -0700

    Added array check for invalid nested elements within a content-body

--- modules/log/checkContentBody.js
+++ modules/log/checkContentBody.js
@@ -1,16 +1,26 @@
 export function checkContentBody(document, filePath, errors) {
+
+  // List of invalid nested elements
+  const nestedElements = ['.content-body', 'header']; 
+		// Add elements that should not be nested in a .content-body to the array above as needed
+			// Prefix classes with '.' to add
+			// Prefix IDs with '#' to add 
+			// Do not prefix tags to add
+
   // Get all elements with the class 'content-body' from the document
   let contentBodies = Array.from(document.querySelectorAll('.content-body'));
 
   // Check each content body
   contentBodies.forEach(contentBody => {
-    // Check if the content body is nested within another content body
-    if (contentBody.querySelectorAll('.content-body').length > 0) {
-      if (!errors[filePath]) {
-        errors[filePath] = [];
+    nestedElements.forEach(nestedElement => {
+      // Check if the content body contains any invalid nested elements
+			if (contentBody.querySelectorAll(nestedElement).length > 0) {
+        if (!errors[filePath]) {
+          errors[filePath] = [];
+        }
+        errors[filePath].push(`An invalid '${nestedElement}' is nested within a '.content-body'`);
       }
-      errors[filePath].push("An invalid '.content-body' (nested within another '.content-body')");
-    }
+    });
 
     // Check if the content body is inside #content-wrapper, #second-column, or #third-column
     let parent = contentBody.parentElement;
