0c0f60f4b776b522bb27cc7d9eb6c8736a54f9a9
Clint Mossman <cmossman@pima.edu>
2024-11-13 16:42:26 -0700

    Added check for vertical tables

--- modules/log/checkTables.js
+++ modules/log/checkTables.js
@@ -1,3 +1,8 @@
+import { config } from '../../config.js';
+
+const vertTableClasses = config.vertTableClasses;
+
+
 export function checkTables(document, filePath, errors) {
   // Get all tables from the document
   let tables = document.querySelectorAll('table');
@@ -6,10 +11,16 @@ export function checkTables(document, filePath, errors) {
   tables.forEach(table => {
     // Check if the table has the 'display-lg' class
     if (!table.classList.contains('display-lg')) {
-      if (!errors[filePath]) {
+
+			// Check if the table has any of the specified classes for vertical tables
+			if(!vertTableClasses.some(className => table.classList.contains(className))) {
+
+			// If the table does not have the 'display-lg' class or vertical table class, add an error
+			if (!errors[filePath]) {
         errors[filePath] = [];
       }
       errors[filePath].push('A table does not contain \'.display-lg\'');
+		} 
     }
 
     // Check the structure of the table
@@ -29,7 +40,9 @@ export function checkTables(document, filePath, errors) {
         }
         errors[filePath].push('A table does not contain the correct structure (missing <tr> within <thead>)');
       }
-    } else {
+    } else if(vertTableClasses.some(className => table.classList.contains(className))) {
+			// Check if the table has any of the specified classes for vertical tables, if not, then 
+		} else {
       if (!errors[filePath]) {
         errors[filePath] = [];
       }
