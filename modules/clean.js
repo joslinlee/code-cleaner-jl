9d6ed96678f89ddb00fbf157958f3fc8529d8e4b
Clint Mossman <cmossman@pima.edu>
2024-11-14 11:35:10 -0700

    Added removeContentBodyInnerTags function to clean file

--- modules/clean.js
+++ modules/clean.js
@@ -8,6 +8,7 @@ import { removeTargetAttributes } from "./clean/removeTargetAttributes.js";
 import { removeRolePresentation } from "./clean/removeRolePresentation.js";
 import { matchTitleToH1 } from "./clean/matchTitleToH1.js";
 import { cleanImageAttributes } from "./clean/cleanImageAttributes.js";
+import { removeContentBodyInnerTags } from "./clean/removeContentBodyInnerTags.js";
 
 export function clean() {
   gulp.task("clean", async () => {
@@ -16,6 +17,7 @@ export function clean() {
       .pipe(cleanTextElements())
       .pipe(cleanElementAttributes())
       .pipe(cleanTableAttributes())
+			.pipe(removeContentBodyInnerTags())
       .pipe(removeTargetAttributes())
       .pipe(removeRolePresentation())
       .pipe(matchTitleToH1())
