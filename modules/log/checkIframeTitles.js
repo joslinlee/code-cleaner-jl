19b9a97659fc20dd19b2ed4b41975433fbd10446
aphan-webdev <aphan@pima.edu>
2024-11-14 17:42:20 -0700

    Import variables from config

--- modules/log/checkIframeTitles.js
+++ modules/log/checkIframeTitles.js
@@ -1,9 +1,11 @@
-export function checkIframeTitles(document, filePath, errors, stringsToCheck, urlsToExclude) {
+import { config } from "../../config.js";
+
+const titlesToCheck = config.titlesToCheck;
+const iframesToExclude = config.iframesToExclude;
+
+export function checkIframeTitles(document, filePath, errors) {
   // Get all iframes from the document
   let iframes = Array.from(document.querySelectorAll('iframe'));
-  stringsToCheck = ["YouTube video player"];
-  // Skips YouTube placeholder iframes
-  urlsToExclude = ["https://www.youtube.com/embed/NpEaa2P7qZI?si=DFcFec4auMcyTLXX"];
 
   // Check each iframe
   iframes.forEach(iframe => {
@@ -45,9 +47,9 @@ export function checkIframeTitles(document, filePath, errors, stringsToCheck, ur
 
     // If 'media-container' does not have a 'media-info' sibling, check the iframe's title attribute
     if (!hasMediaInfoSibling && title) {
-      stringsToCheck.forEach(str => {
+      titlesToCheck.forEach(str => {
         // Exclude Youtube placeholder iframes from log since those have the default title attr
-        if (!urlsToExclude.some(url => src && src.includes(url)) && title.includes(str)) {
+        if (!iframesToExclude.some(url => src && src.includes(url)) && title.includes(str)) {
           if (!errors[filePath]) {
             errors[filePath] = [];
           }
