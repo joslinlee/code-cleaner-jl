66d7a782e0300b4ba5add21e3487d0062b10c8cc
aphan-webdev <aphan@pima.edu>
2024-11-19 17:01:08 -0700

    Add comments

--- modules/log/checkContentWrapper.js
+++ modules/log/checkContentWrapper.js
@@ -1,7 +1,9 @@
 export function checkContentWrapper(document, filePath, errors) {
   // Check if the document contains a div with the id 'content-wrapper'
   let contentWrapper = document.querySelector('div#content-wrapper');
+  // Check if document contains #first-column
   let firstColumn = document.querySelector('div#first-column');
+  // If neither contentWrapper nor firstColumn exists, it will log the error
   if (!contentWrapper && !firstColumn) {
     // Initialize the errors array for the file path if it doesn't exist
     if (!errors[filePath]) {
