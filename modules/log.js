eb5f52fd9619bd3c779b03a9bc4be8485ecdad98
Clint Mossman <cmossman@pima.edu>
2025-09-16 15:25:00 -0700

    Refactored log to use grouped checks and handle new errors

--- modules/log.js
+++ modules/log.js
@@ -1,98 +1,92 @@
 import gulp from 'gulp';
 import { JSDOM as jsdom } from 'jsdom';
+import { decode } from "whatwg-encoding";
 import through2 from 'through2';
 import fs from 'fs';
 import path from 'path';
-import { checkContentBody } from "./log/checkContentBody.js"
-import { checkHead } from "./log/checkHead.js"
-import { checkDeprecated } from "./log/checkDeprecated.js"
-import { checkHeader } from "./log/checkHeader.js"
-import { checkFirstColumn } from './log/checkFirstColumn.js';
-import { checkMedia } from './log/checkMedia.js';
-import { checkIframeOnlyPages } from './log/checkIframeOnlyPages.js';
-import { checkIframeTitles } from './log/checkMedia/checkIframeTitles.js';
-import { checkText } from './log/checkText.js';
+// Import the single function that runs all checks
+import { runAllChecks } from './log/runChecks.js';
 
-const logTitleMessage = "Log Report for Course Review"
+const logTitleMessage = "Log Report for Course Review";
 
 export function log() {
-  let errors = {};
+  const reportsLog = [];
+  reportsLog.push(logTitleMessage);
+  reportsLog.push(`${new Date().toLocaleString()}`);
+  reportsLog.push('--------------------------------------------------');
+  let filesWithErrors = 0;
 
-  gulp.task('log', () => 
-    gulp.src('_input/**/*.{html,htm}')
-      .pipe(through2.obj(function(file, enc, callback) {
+  gulp.task('log', () => {
+    return gulp
+      .src('_input/**/*.{html,htm}')
+      .pipe(through2.obj(function (file, enc, callback) {
         if (file.isBuffer()) {
-          let content = file.contents.toString();
-          let dom = new jsdom(content, { includeNodeLocations: true });
-          let document = dom.window.document;
+          // Decode the buffer to a string. whatwg-encoding will strip the BOM
+          // and use it to detect encoding, falling back to utf-8.
+          const content = decode(file.contents, "UTF-8");
+          const dom = new jsdom(content, { includeNodeLocations: true });
+          const document = dom.window.document;
 
-          // Check if the page contains only <iframe> elements
-          const isIframeOnlyPage = checkIframeOnlyPages(document);
+          const errorsForFile = [];
+          const tempErrorsContainer = { [file.path]: errorsForFile };
 
-          if (isIframeOnlyPage) {
-            // Only run iframe title check
-            checkIframeTitles(document, file.path, errors);
-          } else {
-            // Run all checks
-						checkHead(document, file.path, errors);
-            checkHeader(document, file.path, errors);
-            checkFirstColumn(document, file.path, errors);
-            checkContentBody(document, file.path, errors);
-            checkDeprecated(document, file.path, errors);
-						checkMedia(document, file.path, errors);
-						checkText(document, file.path, errors);
+          runAllChecks({
+            document,
+            filePath: file.path,
+            errors: tempErrorsContainer,
+            html: content,
+          });
+
+          if (errorsForFile.length > 0) {
+            filesWithErrors++;
+            const r = path.relative('_input', file.path);
+            const fileErrors = [`${filesWithErrors}. Errors in file "${r}":`];
+            console.log(`${filesWithErrors}. Errors in file "${r}":`);
+
+            for (const errorObj of errorsForFile) {
+              let message;
+              if (typeof errorObj === 'string') {
+                message = errorObj;
+              } else if (errorObj && typeof errorObj.line === 'number') {
+                message = `${errorObj.message} (line ${errorObj.line})`;
+              } else if (errorObj && errorObj.node) {
+                const location = dom.nodeLocation(errorObj.node);
+                if (!location) {
+                  // For debugging: log when a node is passed but has no location info.
+                  console.log(`[DEBUG] log.js: Node for error "${errorObj.message}" has no location. Node outerHTML:`, errorObj.node.outerHTML);
+                }
+                message = location
+                  ? `${errorObj.message} (line ${location.startLine})`
+                  : errorObj.message;
+              } else {
+                message = (errorObj && errorObj.message) || 'Unknown error structure';
+              }
+              console.log(` > ${message}`);
+              fileErrors.push(` > ${message}`);
+            }
+
+            console.log('--------------------------------------------------');
+            reportsLog.push(fileErrors.join('\n'));
           }
-
-          file.contents = Buffer.from(dom.serialize());
         }
-        callback(null, file);
-      }))
-      .pipe(through2.obj(function(file, enc, callback) {
+
         callback(null, file);
       }))
       .on('finish', () => {
-        let hasErrors = false;
-        let t = 1;
-
-				const reportsLog = [];
-				reportsLog.push(logTitleMessage);
-				reportsLog.push(`${new Date().toLocaleString()}`);
-				reportsLog.push('--------------------------------------------------');
-      
-        for (let e in errors) {
-          if (errors[e].length > 0) {
-            let r = path.relative('_input', e);
-            console.log(`${t}. Errors in file "${r}":`);
-						let fileErrors = [`${t}. Errors in file "${r}":`];
-            for (let n of errors[e]) {
-              console.log(` > ${n}`);
-							fileErrors.push(` > ${n}`);
-            }
-            console.log('--------------------------------------------------');
-						reportsLog.push(fileErrors.join('\n'));
-            t++;
-            hasErrors = true;
-          }
-        }
-      
-        if (!hasErrors) {
-					if (!hasErrors) {
-						reportsLog.push(`--------------------------------------------------\nNo errors found.\n--------------------------------------------------`);
-					}
-          console.log(`--------------------------------------------------
+        if (filesWithErrors === 0) {
+          const noErrorsMessage = `--------------------------------------------------
 No errors found.
---------------------------------------------------`);
+--------------------------------------------------`;
+          reportsLog.push(noErrorsMessage);
+          console.log(noErrorsMessage);
         }
 
-				 // Write to a .txt file (or .json if you'd rather serialize it)
-				 const reportsPath = "./_reports/log-output.txt";
+        // Write to a .txt file (or .json if you'd rather serialize it)
+        const reportsPath = "./_reports/log-output.txt";
 
-				 if (!fs.existsSync("_reports")) {
-					fs.mkdirSync("_reports");
-				}
-
-				 fs.writeFileSync(reportsPath, reportsLog.join('\n\n'), 'utf8');
-				 console.log(`Log written to: ${reportsPath}`);
-      })            
-  );
+        fs.mkdirSync("_reports", { recursive: true });
+        fs.writeFileSync(reportsPath, reportsLog.join('\n\n'), 'utf8');
+        console.log(`Log written to: ${reportsPath}`);
+      });
+  });
 }
