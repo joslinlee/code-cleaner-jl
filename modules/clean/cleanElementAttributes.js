ab59d971ca8f8c8cb6484204b6eaa1a9aaf4d595
Clint Mossman <cmossman@pima.edu>
2025-09-24 10:57:21 -0700

    Change parsing for element cleaning and added comments

--- modules/clean/cleanElementAttributes.js
+++ modules/clean/cleanElementAttributes.js
@@ -1,3 +1,7 @@
+/**
+ * @file Defines a Gulp transformation to remove specified attributes (e.g., 'style')
+ * from various block-level elements like divs and spans.
+ */
 import { domTransform } from "../hooks/domTransform.js";
 import { config } from "../../config.js";
 
@@ -6,8 +10,8 @@ export function cleanElementAttributes() {
   return domTransform((document) => {
     const discardElemAttributes = (element, ...attributes) =>
       attributes.forEach((attribute) => element.removeAttribute(attribute));
-    return document.querySelectorAll(config.noStyleElements).forEach((elem) =>
-      discardElemAttributes(elem, config.styleRemoval[0])
+    return document.querySelectorAll(config.noStyleElements[0]).forEach((elem) =>
+      discardElemAttributes(elem, config.styleRemoval)
     );
   });
 }
