b8034bf741a62078a626c1de9b3d275f1f0b8c35
Clint Mossman <cmossman@pima.edu>
2025-09-24 10:57:50 -0700

    Added spread operator for config and added comments

--- modules/clean/cleanTableAttributes.js
+++ modules/clean/cleanTableAttributes.js
@@ -1,13 +1,17 @@
+/**
+ * @file Defines a Gulp transformation to remove deprecated or styling attributes
+ * from table-related elements (<table>, <th>, <td>, etc.), such as
+ * 'cellspacing', 'cellpadding', 'width', and 'style'.
+ */
 import { domTransform } from "../hooks/domTransform.js";
 import { config } from "../../config.js";
 
-// remove given attributes from table elements
 export function cleanTableAttributes() {
   return domTransform((document) => {
     const discardTableAttributes = (tableElement, ...tableAttributes) =>
       tableAttributes.forEach((tableAttribute) => tableElement.removeAttribute(tableAttribute));
     return document.querySelectorAll(config.tableElementsSelector).forEach((tableElem) =>
-      discardTableAttributes(tableElem, config.tableAttributesToRemove)
+      discardTableAttributes(tableElem, ...config.tableAttributesToRemove)
     );
   });
 }
